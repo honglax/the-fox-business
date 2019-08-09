b157f47289ac34c97246a773f6c9efaa720db5d2
Hong La <hongla.html@gmail.com>
2019-08-09 15:12:30 +0700

    http://redmine.appscyclone.com/issues/42205 Implement Sticky Header - Done

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,45 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
 import "../../styles/header.scss";
 import logo from "../../assets/logo-retina.png";
 
-function Header() {
-  return (
-    <header className="header page__header">
-      <Container>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  handleScroll() {
+    this.setState({ scroll: window.scrollY });
+  }
+
+  componentDidMount() {
+    const el = document.querySelector("nav");
+    this.setState({ top: el.offsetTop, height: el.offsetHeight });
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentDidUpdate() {
+    const el = document.querySelector("nav");
+    let classNames = el.className.split(" ");
+    if (this.state.scroll > 0) {
+      document.body.style.paddingTop = `${this.state.height}px`;
+      if (!classNames.includes("fixed-nav")) {
+        el.className += " fixed-nav";
+      }
+    } else {
+      document.body.style.paddingTop = 0;
+      el.className = classNames
+        .filter(className => className !== "fixed-nav")
+        .join(" ");
+    }
+  }
+
+  render() {
+    return (
+      <header className="header page__header">
         <Navbar collapseOnSelect expand="lg">
-          <NavLink className="navbar-brand" to="/">
-            <img src={logo} alt="The Fox Business" className="logo" />
-          </NavLink>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
-              <NavLink className="nav-link" exact to="/">
-                Home
-              </NavLink>
-              <NavLink className="nav-link" to="/about">
-                About Us
-              </NavLink>
-              <NavLink className="nav-link" to="/services">
-                Services
-              </NavLink>
-              <NavLink className="nav-link" to="/blog">
-                Blog
-              </NavLink>
-              <NavLink className="nav-link" to="/portfolio">
-                Portfolio
-              </NavLink>
-              <NavLink className="nav-link" to="/contact">
-                Contact
-              </NavLink>
-            </Nav>
-          </Navbar.Collapse>
+          <Container>
+            <NavLink className="navbar-brand" to="/">
+              <img src={logo} alt="The Fox Business" className="logo" />
+            </NavLink>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto">
+                <NavLink className="nav-link" exact to="/">
+                  Home
+                </NavLink>
+                <NavLink className="nav-link" to="/about">
+                  About Us
+                </NavLink>
+                <NavLink className="nav-link" to="/services">
+                  Services
+                </NavLink>
+                <NavLink className="nav-link" to="/blog">
+                  Blog
+                </NavLink>
+                <NavLink className="nav-link" to="/portfolio">
+                  Portfolio
+                </NavLink>
+                <NavLink className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
         </Navbar>
-      </Container>
-    </header>
-  );
+      </header>
+    );
+  }
 }
 
 export default Header;
