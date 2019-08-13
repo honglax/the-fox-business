dea72b39ac9dbd1327659728010f2e72f7a36255
Hong La <hongla.html@gmail.com>
2019-08-13 10:47:04 +0700

    http://redmine.appscyclone.com/issues/42205 [Sticky Header] Fix banner height when resize viewport

--- src/app/components/StyledComponent/Slideshow.js
+++ src/app/components/StyledComponent/Slideshow.js
@@ -1,38 +1,29 @@
 import React, { Component } from "react";
 
 class SlideShow extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-
-    // this.handleResize = this.handleResize.bind(this);
-  }
-
-  // handleResize() {
-  //   if (window.matchMedia("(max-width: 991.98px)").matches) {
-  //     const banner = document.getElementById("slideshow");
-  //     banner.children[0].style.height = `${window.innerHeight - 80}px`;
-  //   }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
   // }
 
-  componentDidMount() {
+  handleSetHeight() {
     const banner = document.getElementById("slideshow");
     const header = document.querySelector("header");
     if (window.matchMedia("(max-width: 991.98px)").matches) {
-      const banner = document.getElementById("slideshow");
       banner.children[0].style.height = `${window.innerHeight - 80}px`;
     } else {
       banner.children[0].style.height = `${window.innerHeight -
         header.offsetHeight}px`;
     }
-    // window.addEventListener("resize", this.handleResize);
+  }
+
+  componentDidMount() {
+    this.handleSetHeight();
+    window.addEventListener("resize", this.handleSetHeight);
   }
 
   componentDidUpdate() {
-    if (window.matchMedia("(max-width: 991.98px)").matches) {
-      const banner = document.getElementById("slideshow");
-      banner.children[0].style.height = `${window.innerHeight - 80}px`;
-    }
+    this.handleSetHeight();
   }
 
   render() {
